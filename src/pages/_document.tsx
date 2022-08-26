14e3e9c152cc2ab5184c4ca596524f22f194bad1
Rickard Lindahl <mail@rickardlindahl.com>
2022-08-26 15:41:07 +0200

    Add daisy-ui

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html data-theme="emerald">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
