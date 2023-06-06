90ef2482ab94eecc93e27291fbfb221a4b1ce351
Rickard Lindahl <rickard@lindahl.app>
2023-06-06 07:22:57 +0200

    Display logged in user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+  const user = await currentUser();
+
   return (
     <main>
       <h1>Salen Samfällighetsförening</h1>
+      <div>Inloggad som: {user.firstName} {user.lastName}</div>
     </main>
   );
 }
