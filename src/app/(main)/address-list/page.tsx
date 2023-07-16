6f722348f32564f4e6c85ea410f987e078c80bf6
Rickard Lindahl <rickard@lindahl.app>
2023-07-16 12:31:07 +0200

    Add /address-list page WIP

---
+++ src/app/(main)/address-list/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "~/components/container";
+
+export default function AddressListPage() {
+  return (
+    <Container className="mt-24 md:mt-28">
+      <h1>Addresslista</h1>
+      <p>Kommer snart!</p>
+    </Container>
+  );
+}
