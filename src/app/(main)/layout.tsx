267f706c4a20d13c3f60884bd4064c3b32e3734f
Rickard Lindahl <rickard@lindahl.app>
2023-07-24 21:29:13 +0200

    Make index route public

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 import { MainNav } from "~/components/main-nav";
 import { SiteFooter } from "~/components/site-footer";
+import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
 import { mainNav } from "~/settings";
 
 export default function MainLayout({
@@ -15,7 +17,19 @@ export default function MainLayout({
         <div className="flex h-20 items-center justify-between py-6">
           <MainNav items={mainNav} />
           <nav>
-            <UserButton />
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <button
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4",
+                )}
+              >
+                <SignInButton>Logga in</SignInButton>
+              </button>
+            </SignedOut>
           </nav>
         </div>
       </header>
